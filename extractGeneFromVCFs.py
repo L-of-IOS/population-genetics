9dde52d4832a0385faf8d21a946a2840bce82dbe
L-of-IOS <78941660+L-of-IOS@users.noreply.github.com>
2021-03-18 10:31:23 +0800

    Update extractGeneFromVCFs.py

--- extractGeneFromVCFs.py
+++ extractGeneFromVCFs.py
@@ -7,9 +7,17 @@ TIPS:
 3. write chr pos function in new file
 
 """
+class My_window:
+    def __init__(self,i,CHR,bin_in,bin_out):
+        self.CHR = CHR
+        self.ind = i
+        self.bin_in = bin_in
+        self.bin_out = bin_out
 
+    
+    
 
-
+windows = []
 
 import argparse
 import gzip
@@ -24,8 +32,53 @@ parser.add_argument("-g", "--gzip", action="store_true", required=False, help="S
 args = parser.parse_args()
 vcf_in = args.vcf
 out_name = args.out
+win_input = args.input
 
 if args.gzip:
     opener = gzip.open
 else:
     opener = open
+
+
+
+with open(win_input,mode = 'r') as win_file:    
+    for row in win_file:
+        if "chrom\t" in row:
+            continue
+        else :
+            CHR,bin_in,bin_out = row.split()[0:3]
+            windows.append(My_window(i = len(windows),
+		CHR = CHR,bin_in = bin_in,bin_out = bin_out))
+
+##sort class
+windows = sorted(windows,key = lambda x:  (x.CHR,x.bin_in))
+
+out_file = open(out_name,mode = 'w')
+out_file.write("CHROM\tpos\tfunction\n")
+
+with opener(vcf_in, 'r') as tsvin:
+        tsvin = csv.reader(tsvin, delimiter='\t')
+
+        for row in tsvin:
+            if any('##' in strings for strings in row):
+                continue
+            if any('#CHROM' in strings for strings in row):
+                continue
+            chrom,pos,id,ref,alt,qual,filter,info,format=row[0:9]
+            haplotypes = row[9:]            
+            if not any(chrom  in i.CHR for i in windows):
+                continue
+            else:
+                if all(int(pos) < k for k in [int(i.bin_in) for i in windows if chrom == i.CHR ]):
+                    continue
+                elif all(int(pos) < k for k in [int(i.bin_out) for i in windows if chorom ==i.CHR and int(i.bin_in) >= int(pos)]) :
+                    ##if  bin_in<pos<bin_out
+                    notes = info.split("|")[1:]
+                    notes = '|'.join(notes)
+                    out_file.write(chrom+"\t"+str(pos)+"\t"+notes)
+                else:continue
+
+
+
+
+
